ebe961495d56e97432bb9520066c7efa40d5ed88
Steven Walton <walton.stevenj@gmail.com>
2020-11-11 13:44:16 -0800

    Commit main to create checkpoint before merging luis's

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
+#include <iostream>
+#include <vector>
 #include "nn.h"
+#include "types.h"
 
 int main()
 {
-    LinearLayer l0(2,3);
-    LinearLayer l1(3,3);
-    l0.Forward();
-    l1.Connect(l0.Get_Output());
+    LinearLayer l0(1,2);
+    LinearLayer l1(2,1);
+    std::vector<struct node> n;
+    node nn;
+    nn.activation = 1;
+    nn.weight.push_back(1);
+
+    n.emplace_back(nn);
+    l0.Forward(n, 0);
+    //l1.Connect(l0.Get_Output());
 
     return 0;
 }
