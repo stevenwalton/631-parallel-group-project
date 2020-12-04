b6f3c75da2f139d277c1a6eee4b737d458707d5c
lfguzman <lguzmann@uoregon.edu>
2020-12-03 18:26:16 -0600

    Moved datasets to independent folder

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ int main(int argc, const char * argv[])
         cout << "Learning rate = 0.3\n";
         cout << "Epochs = 500\n";
 	cout << "Batch size = 1\n\n";
-        dataset = "moons_dataset.txt";
+        dataset = "datasets/moons_dataset.txt";
     	learning_rate = 0.3f;
         n_epochs = 100;
         batch_size = 32;
